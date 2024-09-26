076d0e81087d9d5d35d82d0cb8c505f7894de443
anddy-agudelo <andiagudelo@gmail.com>
2024-09-25 22:03:04 -0500

    add db connection

--- core/config.py
+++ core/config.py
@@ -2,6 +2,7 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
+    ENV: str
     DB_CONNECTION: str
     DB_NAME: str
     API_STR: str = "/api"
--- main.py
+++ main.py
@@ -1,10 +1,26 @@
+from contextlib import asynccontextmanager
+
 import uvicorn
 from fastapi import FastAPI
+from motor.motor_asyncio import AsyncIOMotorClient
 
 from api.users.controllers.users_controller import users_router
 from core.config import settings
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("Application starting...")
+    env = settings.ENV
+    app.mongodb_client = AsyncIOMotorClient(settings.DB_CONNECTION)
+    app.database = app.mongodb_client[settings.DB_NAME]
+    print(f"Started successfully: {env.value}")
+
+    yield
+    print("Application closing...")
+    app.mongodb_client.close()
+
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(users_router, prefix=settings.API_STR)
 
